ccff9e27f0938646756bf494fff57863b6017c6a
Michael Born <michael@ortussolutions.com>
2024-06-12 10:58:35 -0400

    More JDBC testing

--- run.sh
+++ run.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
-# java -cp resources/libs/ ortus.boxlang.runtime.BoxRunner
-java -cp resources/libs/* ortus.boxlang.runtime.BoxRunner
+BOXLANG_REPO_HOME=~/repos/boxlang
+BOXLANG_MODULE_INSTALL_DIR=~/.boxlang/modules
+
+rm -rf bin lib
+
+if [ ! -d "bin" ]; then
+    # Setup the miniserver binaries
+    cp $BOXLANG_REPO_HOME/boxlang-miniserver/build/distributions/boxlang-miniserver-1.0.0.zip .
+    unzip boxlang-miniserver-1.0.0.zip
+fi
+
+# Copy/install any custom module builds to your .boxlang/modules directory
+unzip -u -o $BOXLANG_REPO_HOME/modules/bx-compat/build/distributions/bx-compat-1.0.0.zip -d $BOXLANG_MODULE_INSTALL_DIR/bx-compat
+unzip -u -o $BOXLANG_REPO_HOME/modules/bx-mssql/build/distributions/bx-mssql-1.0.0.zip -d $BOXLANG_MODULE_INSTALL_DIR/bx-mssql
+
+./bin/boxlang-miniserver
